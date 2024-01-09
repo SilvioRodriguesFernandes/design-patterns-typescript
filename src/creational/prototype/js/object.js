7c95eae9077c9f26f6ec2654b800cb962e8b6bb9
Silvio Rodrigues Fernandes <107973623+SilvioRodriguesFernandes@users.noreply.github.com>
2024-01-08 21:17:15 -0400

    Prototype

---
+++ src/creational/prototype/js/object.js
@@ -0,0 +1,15 @@
+const personPrototype = {
+  firstName: 'Karina',
+  lastName: 'Lima',
+  age: 22,
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+
+const anotherPerson = Object.create(personPrototype);
+anotherPerson.firstName = 'Aline';
+console.log(anotherPerson);
+console.log(anotherPerson.firstName);
+console.log(anotherPerson.fullName());
---
+++ src/creational/prototype/prototype-shallowcopy/prototype-shallowcopy.ts
@@ -0,0 +1,37 @@
+export interface Prototype {
+  clone(): Prototype;
+}
+
+export class Person implements Prototype {
+  public addresses: Address[] = [];
+
+  constructor(
+    public name: string,
+    public age: number,
+  ) {}
+
+  clone(): this {
+    const newObj = Object.create(this);
+    return newObj;
+  }
+
+  addAddress(address: Address): void {
+    this.addresses.push(address);
+  }
+}
+
+export class Address {
+  constructor(
+    public street: string,
+    public number: number,
+  ) {}
+}
+
+const address1 = new Address('Av. Carlos Gomes', 785);
+const person1 = new Person('Carla', 22);
+person1.addAddress(address1);
+const person2 = person1.clone();
+
+person2.name = 'Bruna';
+console.log(person2);
+console.log(person2.addresses);
